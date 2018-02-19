974f629e5d7b65d4433db4579e056662fcdd0970
Olivier <devangularjs@gmail.com>
2018-02-19 14:17:57 +0100

    feat(table): Ajout table et sort

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -45,4 +45,8 @@ export class TrainingService {
   getRunningExercise() {
     return { ...this.runningExercise };
   }
+
+  getCompletedOrCancelledExercises() {
+    return this.exercises.slice();
+  }
 }
