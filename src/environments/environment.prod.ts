bad357f7e5645877ceadd5243ed604b08d6a79f1
Olivier <devangularjs@gmail.com>
2018-02-20 00:52:33 +0100

    fix prod deploiement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBi5pMpGAUHYafwFAwbCQtupko6u7v02oQ',
+    authDomain: 'ng-fitness-tracker-ec675.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-ec675.firebaseio.com',
+    projectId: 'ng-fitness-tracker-ec675',
+    storageBucket: 'ng-fitness-tracker-ec675.appspot.com',
+    messagingSenderId: '458391142147'
+  }
 };
